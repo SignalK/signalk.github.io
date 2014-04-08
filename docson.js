ddf05d7cea6d2116c35f762dfc92b553b57f4f8d
robert <robert@42.co.nz>
2014-04-08 22:26:54 +1200

    Fix undefined item

--- docson.js
+++ docson.js
@@ -73,7 +73,7 @@ define(["lib/jquery", "lib/handlebars", "lib/highlight", "lib/jsonpointer", "lib
     });
 
     Handlebars.registerHelper('contains', function(arr, item, options) {;
-        if(item && arr && arr.indexOf(item) != -1) {
+        if(arr && arr.indexOf && arr.indexOf(item) != -1) {
             return options.fn(this);
         }
     });

2804d906d7d41251b20f83ac2c9a96a7bc357a01
Tim Mathews <tmathews04@gmail.com>
2015-04-12 00:24:37 -0400

    Improve layout for medium resolution

--- js/init.js
+++ js/init.js
@@ -9,9 +9,9 @@ function init(base) {
     reset: 'full',
     breakpoints: {
       'global': { range: '*', href: base + '/css/style.css' },
-      'desktop':{ range: '737-', href: base + '/css/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
-      '1000px': { range: '737-1200', href: base + '/css/style-1000px.css', containers: 1000, grid: { gutters: 20 }, viewport: { width: 1080 } },
-      'mobile': { range: '-736', href: base + '/css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
+      'desktop':{ range: '1200-', href: base + '/css/style-desktop.css', containers: 1200, grid: { gutters: 25 } },
+      '1000px': { range: '1010-1200', href: base + '/css/style-1000px.css', containers: 1000, grid: { gutters: 20 }, viewport: { width: 1080 } },
+      'mobile': { range: '-735', href: base + '/css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
     },
     plugins: {
       layers: {

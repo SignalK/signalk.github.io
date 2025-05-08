274d8391ce6b66ce78e4f1b1fe0794cb64583db5
Brandon Keepers <brandon@opensoul.org>
2025-05-08 15:30:27 -0400

    Improve npm data loader, cache npm responses

--- src/content/appstore.ts
+++ src/content/appstore.ts
@@ -1,70 +1,133 @@
 import { z, defineCollection } from 'astro:content';
+import doFetch from 'make-fetch-happen';
+import { join } from 'node:path';
+import type { LoaderContext } from 'astro/loaders';
+import pMap from 'p-map';
 
-export default defineCollection({
-  schema: z.object({
-    id: z.string(),
-    package: z.object({
-      name: z.string(),
-      description: z.string(),
-      version: z.string(),
-      keywords: z.array(z.string()),
-      publisher: z.object({
+const cachePath = join(import.meta.dirname, '../../node_modules/.cache/fetch')
+
+export const fetch = doFetch.defaults({
+  cachePath,
+});
+
+const schema = z.object({
+  package: z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    version: z.string(),
+    keywords: z.array(z.string()),
+    publisher: z.object({
+      username: z.string(),
+      email: z.string(),
+    }),
+    maintainers: z.array(
+      z.object({
         username: z.string(),
         email: z.string(),
-      }),
-      maintainers: z.array(
-        z.object({
-          username: z.string(),
-          email: z.string(),
-        })
-      ),
-      license: z.string().optional(),
-      date: z.string().datetime(),
-      links: z.record(z.string(), z.string())
-    }),
-    downloads: z.object({
-      monthly: z.number(),
-      weekly: z.number(),
-    }),
-    updated: z.string(),
-    score: z.object({
-      final: z.number(),
-      detail: z.object({
-        popularity: z.number(),
-        quality: z.number(),
-        maintenance: z.number(),
-      }),
-    }),
-    flags: z.object({
-      insecure: z.number(),
-    })
+      })
+    ),
+    license: z.string().optional(),
+    date: z.string().datetime(),
+    links: z.record(z.string(), z.string())
   }),
+  downloads: z.object({
+    monthly: z.number(),
+    weekly: z.number(),
+  }),
+  updated: z.string(),
+  score: z.object({
+    final: z.number(),
+    detail: z.object({
+      popularity: z.number(),
+      quality: z.number(),
+      maintenance: z.number(),
+    }),
+  }),
+  flags: z.object({
+    insecure: z.number(),
+  }),
+  readme: z.string(),
+  versions: z.record(
+    z.string(),
+    z.object({
+      name: z.string(),
+      version: z.string(),
+      description: z.string().optional(),
+      keywords: z.array(z.string()),
+      signalk: z.object({
+        appIcon: z.string().optional(),
+        displayName: z.string().optional(),
+      })
+    })
+  )
+});
 
-  // TODO: update to use content loader api
-  // https://docs.astro.build/en/reference/content-loader-reference/
-  loader: async () => {
-    const keywords = [
+export type Module = z.infer<typeof schema>;
+
+export default defineCollection({
+  schema,
+  loader: npmLoader({
+    keywords: [
       'signalk-node-server-plugin',
       'signalk-embeddable-webapp',
       'signalk-webapp'
-    ];
+    ]
+  })
+})
 
-    // TODO paginate
+export function npmLoader({ keywords = [], concurrency = 10 }: { keywords?: string[], concurrency?: number } = {}) {
+  return {
+    name: 'npm',
+    load: async ({ store, logger, meta }: LoaderContext): Promise<void> => {
+      logger.info(`Loading npm modules with keywords: ${keywords.join(', ')}`);
+
+      await pMap(keywords, (async (keyword) => {
+        const modules = await fetchModulesByKeyword(keyword);
+        await pMap(modules, (async (module) => {
+          const id = module.package.name;
+
+          logger.debug(`Fetching ${module.package.name}`);
+          const res = await fetch(`https://registry.npmjs.org/${id}`);
+          if (!res.ok) throw new Error(`Failed to fetch package data: ${res.statusText}`);
+          const data = await res.json();
+
+          meta.set("lastModified", module.package.date)
+
+          store.set({
+            id: module.package.name,
+            data: {
+              ...module,
+              ...data
+            },
+          })
+        }), { concurrency });
+      }), { concurrency });
+    }
+  }
+}
+
+async function fetchModulesByKeyword(keyword: string): Promise<Module[]> {
+  const items: Module[] = []
+  const size = 250;
+
+  while (true) {
     const url = new URL('https://registry.npmjs.org/-/v1/search');
     url.search = new URLSearchParams({
-      size: '250',
-      from: '0',
-      text: `keywords:${keywords[0]}`
+      size: size.toString(),
+      from: items.length.toString(),
+      text: `keywords:${keyword}`
     }).toString();
 
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await fetch(url.toString());
+    if (!response.ok) throw new Error(`Failed to fetch modules: ${response.statusText}`);
+    const data = await response.json() as { objects: Module[], total: number };
 
-    const result = data.objects.map((item) => ({
-      id: item.package.name,
-      ...item,
-    }));
+    items.push(...data.objects);
 
-    return result;
-  },
-})
+    if (data.objects.length < 250) {
+      break;
+    }
+  }
+
+  return items;
+}
